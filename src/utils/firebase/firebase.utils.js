272ac430255c641bb53274d025c6215ff4b43526
LordDrakkarxD <araujogalhardo@hotmail.com>
2022-05-05 23:26:09 -0300

    first commit

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5to_m4LwQB9EXPZr3aZgCvk8WRvzkU0Y",
+  authDomain: "ditalia-db.firebaseapp.com",
+  projectId: "ditalia-db",
+  storageBucket: "ditalia-db.appspot.com",
+  messagingSenderId: "1029133655557",
+  appId: "1:1029133655557:web:90515d8fa109cb9fb5866c",
+  measurementId: "G-RXY34KXKSW",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (err) {
+      console.log("error creating the user", err.message);
+    }
+  }
+
+  return userDocRef;
+};
